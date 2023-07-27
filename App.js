cc311b6965bdfed87e539290e68ef28a2811a080
Venkatesh-Sharma29 <122893866+Venkatesh-Sharma29@users.noreply.github.com>
2023-07-27 18:13:09 +0530

    App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Navbar from './components/Navbar';
 import UserGrid from './components/UserGrid';
 import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -10,6 +11,30 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     background-color: #2f2347;
+  }
+`;
+const LoadingCircle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  .loader {
+    border: 4px solid rgba(255, 255, 255, 0.3);
+    border-top: 4px solid #007bff;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    animation: spin 1s linear infinite;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
 `;
 
@@ -33,7 +58,13 @@ const App = () => {
     <>
       <GlobalStyle />
       <Navbar onGetUsers={getUsers} />
-      {isLoading ? <p>Loading...</p> : <UserGrid users={users} />}
+      {isLoading ? (
+        <LoadingCircle>
+          <div className="loader" />
+        </LoadingCircle>
+      ) : (
+        <UserGrid users={users} />
+      )}
     </>
   );
 };
